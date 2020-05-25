6c572f27f963eaf5ec4ddd567d118d4c105ebdc3
simonthi <github@simonthiefes.de>
2020-05-25 11:01:19 +0100

    Updated wording to be less confusing

--- UnicodeReporter.py
+++ UnicodeReporter.py
@@ -7,12 +7,12 @@ Report Glyphs with double and no Unicode assignments.
 import GlyphsApp
 
 Glyphs.showMacroWindow()
-print('___Double Unicodes___')
+print('___Glyphs with more than one Unicodes___')
 for myGlyph in Glyphs.font.glyphs:
 	if myGlyph.unicodes != None:
 		if len(myGlyph.unicodes)>1:
 			print myGlyph.name
-print('___No Unicode Assigned___')
+print('___Glyphs with no Unicode Assigned___')
 for myGlyph in Glyphs.font.glyphs:
 	if myGlyph.unicodes == None:
 		print myGlyph.name

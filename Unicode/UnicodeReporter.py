046608c0eaccf735419ed434c7ad8d575f9a1a72
simonthi <github@simonthiefes.de>
2022-11-21 10:29:30 +0100

    Little correction for printing correctly to macro window

--- Unicode/UnicodeReporter.py
+++ Unicode/UnicodeReporter.py
@@ -11,8 +11,8 @@ print('___Glyphs with more than one Unicodes___\n')
 for myGlyph in Glyphs.font.glyphs:
 	if myGlyph.unicodes != None:
 		if len(myGlyph.unicodes)>1:
-			print myGlyph.name, myGlyph.unicodes
+			print(myGlyph.name, myGlyph.unicodes)
 print('\n \n \n___Glyphs with no Unicode Assigned___\n')
 for myGlyph in Glyphs.font.glyphs:
 	if myGlyph.unicodes == None:
-		print myGlyph.name
+		print(myGlyph.name)
